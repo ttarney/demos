37a03c9915097ea60da9aed62953c6b54044e3f0
ttarneynei <47861323+ttarneynei@users.noreply.github.com>
2019-02-26 14:34:06 -0600

    removed feature 1

--- featuretoggledemo/featuretoggleimpl/BaseToggles.cs
+++ featuretoggledemo/featuretoggleimpl/BaseToggles.cs
@@ -6,9 +6,6 @@ namespace featuretoggleimpl
     public class Printing : SimpleFeatureToggle { }
     public class Saving : SimpleFeatureToggle { }
 
-    public class Feature1 : SimpleFeatureToggle { }
-
-    public class Feature2 : SimpleFeatureToggle { }
 
     
 
