d5147860fc32dfb8b0ebf901fcb55760d752ddde
txt5321 <txt5321@sfgmembers.com>
2022-11-03 09:34:51 -0500

    add

---
+++ FeatureFlagDemoAPI/FeatureFlagDemoAPI/Controllers/BeerController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FeatureFlagDemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("all beers");
+            
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok("a beer");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string value)
+        {
+
+            return Ok("added beer");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] string value)
+        {
+            return Ok("updated beer");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok("deleted a beer");
+        }
+    }
+}
